3a4bd707dff46dcd57b2c5e24ee764bc26323b4b
greats3an <greats3an@gmail.com>
2024-01-17 17:30:21 +0800

    Updated URLs

--- __init__.py
+++ __init__.py
@@ -1,13 +1,13 @@
 bl_info = {
     "name": "SSSekai Blender IO",
-    "author": "SSSekai",
+    "author": "mos9527",
     "version": (0, 0, 1),
     "blender": (4, 0, 0),
     "location": "View3D > Sidebar > SSSekai",
     "description": "Project SEKAI Asset Importer for Blender 4.0+",
     "warning": "",
-    "wiki_url": "https://github.com/mos9527/sssekai",
-    "tracker_url": "https://github.com/mos9527/sssekai",
+    "wiki_url": "https://github.com/mos9527/sssekai_blender_io/wiki",
+    "tracker_url": "https://github.com/mos9527/sssekai_blender_io",
     "category": "Import-Export",
 }
 # Dependencies
