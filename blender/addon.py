d94a9e6d2603cb1ba12e09a2b7216e5b74e8ae7c
mos9527 <greats3an@gmail.com>
2024-10-16 08:47:02 +0800

    Fix deprecated `name` field being used (fixes #10)

--- blender/addon.py
+++ blender/addon.py
@@ -1531,7 +1531,7 @@ class SSSekaiRLAImportPanel(bpy.types.Panel):
                     for obj in rla_env.objects:
                         if obj.type in {ClassIDType.TextAsset}:
                             data = obj.read()
-                            datas[data.name] = data.m_Script.encode(
+                            datas[data.m_Name] = data.m_Script.encode(
                                 "utf-8", "surrogateescape"
                             )
                 header = sssekai_global.rla_header = json.loads(
