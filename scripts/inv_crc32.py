2e5c844d35c7b0c6107c7230bb1bc21d21f84d0d
mos9527 <greats3an@gmail.com>
2025-02-10 09:26:37 +0800

    Update inv_crc32

--- scripts/inv_crc32.py
+++ scripts/inv_crc32.py
@@ -22,12 +22,12 @@ import sys
 
 Col.init()
 logprint = lambda *a, **k: print(*a, **k, file=sys.stderr, sep="")
-pvid = "0074"
+pvid = "0133"
 f = open(
-    f"/Volumes/mos9527弄丢的盘/Reverse/proseka_reverse/assets/live_pv/timeline/{pvid}/light",
+    f"/Volumes/mos9527弄丢的盘/Reverse/proseka_reverse/assets/live_pv/timeline/{pvid}/camera",
     "rb",
 )
-aux_kws = ["r", "g", "b"] + ["x", "y", "z", "w"]
+aux_kws = ["r", "g", "b"] + ["x", "y", "z", "w"] + ["postProcess"]
 kws = [
     # SekaiCharacterAmbientLight
     "ambientColor",
@@ -58,7 +58,48 @@ kws = [
     "fogStart",
     "fogEnd",
     "quality",
-    # Misc
+    # PostEffectV2
+    "PostEffectV2",
+    "quality",
+    "enableSekaiDof",
+    "focusDistance",
+    "transitionRange",
+    "focalRegion",
+    "aperture",
+    "focalLength",
+    "disableForeBokeh",
+    "intensity",
+    "scatter",
+    "newBloomBlend",
+    "brightColor",
+    "darkColor",
+    "brightPosition",
+    "darkPosition",
+    "incidentLightType",
+    "incidentLightColor",
+    "incidentLightPosition",
+    "incidentLightLength",
+    "sat",
+    "satAlpha",
+    "saturationModule",
+    "directionalBlurParamType",
+    "directionalBlurStrength",
+    "directionalBlurDirection",
+    "radialBlurCenterPosition",
+    "solarization",
+    "fadeOut",
+    "fadeOutLerp",
+    "fadeOutBeforeProp",
+    "fadeOutBeforePropLerp",
+    "frontLut",
+    "backLut",
+    "frontLutBlend",
+    "backLutBlend",
+    "frontLutPoint",
+    "frontNonLutPoint",
+    "backLutPoint",
+    "backNonLutPoint",
+    "antialiasingMode",
 ]
 kws = kws + [f"{attr}.{aux}" for attr in kws for aux in aux_kws]
 kws = {crc32(k.encode()): k for k in kws}
@@ -73,14 +114,23 @@ valid_kws, missing_kws = set(), set()
 for anim in filter(lambda obj: obj.type == ClassIDType.AnimationClip, env.objects):
     data = anim.read()
     anim: Animation = read_animation(data)
+    if not anim.Name.startswith("depth_of_field"):
+        continue
     logprint(Col.Fore.WHITE, Col.Style.BRIGHT, data.m_Name, Col.Style.RESET_ALL)
-    for attr in anim.CurvesT[0]:
-        if attr in kws:
-            logprint("\t", Col.Fore.WHITE, attr, "\t", Col.Fore.GREEN, kws[attr])
-            valid_kws.add(attr)
+    for path in anim.CurvesT:
+        if path in kws:
+            logprint("\t", Col.Fore.WHITE, path, "\t", Col.Fore.GREEN, kws[path])
+            valid_kws.add(path)
         else:
-            logprint("\t", Col.Fore.RED, attr, "\tNOT FOUND")
-            missing_kws.add(attr)
+            logprint("\t", Col.Fore.RED, path, "\tNOT FOUND")
+            missing_kws.add(path)
+        for attr in anim.CurvesT[path]:
+            if attr in kws:
+                logprint("\t\t", Col.Fore.WHITE, attr, "\t", Col.Fore.GREEN, kws[attr])
+                valid_kws.add(attr)
+            else:
+                logprint("\t\t", Col.Fore.RED, attr, "\tNOT FOUND")
+                missing_kws.add(attr)
     pass
 logprint(
     Col.Style.RESET_ALL,
